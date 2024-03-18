bcf8b3c51bfe45bf9d4d88decc4beb863b70ba26
Azim <azimreact@gmail.com>
2024-03-18 08:00:16 +0600

    added toggle for sidebar

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Sidebar = () => {
+  const [width, setWidth] = useState(true)
+
   return (
-    <div className='sidebar'>
-      Sidebar
+    <div className={`sidebar ${width ? 'w300' : 'w150'}`}>
+      <div>Sidebar</div>
+      <button className='sidebarWidthBtn' onClick={() => setWidth(!width)}>{width ? 'Close' : 'Open'}</button>
     </div>
   );
 };
